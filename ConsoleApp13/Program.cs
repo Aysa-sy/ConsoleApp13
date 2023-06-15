45067187e558bb32d053f9888b050bd4a608ccc7
aysa-sy <aisa.soleimani83@gmail.com>
2023-06-15 13:06:09 +0330

    Add project files.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please ener a number:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int result = calc(n);
+            Console.WriteLine("product 5 numbers is:" + result);
+        }
+
+        static int calc(int n)
+        {
+            int p = 1;
+            while (n > 0)
+            {
+                int aa = n % 10;
+                if (aa > 5)
+                {
+                    p *= aa;
+                }
+                n /= 10;
+            }
+            return p;
+        }
+    }
+}
